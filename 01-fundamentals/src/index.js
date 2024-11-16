08b0fde2f97dd3dd299eb0e88f7adbab88ed18fd
Senie Calalang <senie.c88@gmail.com>
2024-11-16 17:07:04 +0900

    finished 01-fundamentals section

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,23 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-
 import { books } from './books';
-import Book from './Book';
 
-function BookList() {
+// ** CHALLENGE 2: TITLE CHALLENGE ANSWERS **
+// Created a Title constant to render the Title separately to the Booklist.
+const Title = () => {
+  return <h1>Amazon Best Sellers</h1>;
+};
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book, index) => {
+        // ** CHALLENGE 1: NUMBERS CHALLENGE ANSWERS **
+        return <Book {...book} key={book.id} number={index} />;
+      })}
+    </section>
+  );
+};
+
+const Book = (props) => {
+  const { img, title, author, number } = props;
+
   return (
     <>
-      <h1>amazon best sellers</h1>
-      <section className='booklist'>
-        {books.map((book) => {
-          return <Book {...book} key={book.id} />;
-        })}
+      <section className="book">
+        {/* show the ranking */}
+        <span className="number">{`# ${number + 1}`}</span>
+        <img src={img} alt={title} />
+        <h2>{title}</h2>
+
+        <h4>{author}</h4>
       </section>
     </>
   );
-}
+};
+
+// ** CHALLENGE 2 - TITLE CHALLLENGE ANSWERS
+// Combine the Title and Booklist into an App constant to render both together.
+const App = () => {
+  return (
+    <>
+      <Title />
+      <BookList />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<BookList />);
+root.render(<App />);
